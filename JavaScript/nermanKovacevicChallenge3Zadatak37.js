b12c12f0b0bf4d04930d4dad95da2f9a30863090
Nerman Kovačević <101500895+Nerman7@users.noreply.github.com>
2022-07-12 13:47:47 +0200

    Delete nermanKovacevicChallenge3Zadatak37.js

--- JavaScript/nermanKovacevicChallenge3Zadatak37.js
+++
@@ -1,8 +0,0 @@
-// Program koji racuna prosjek brojeva.
-
-let num1 = 7;
-let num2 = 11;
-let num3 = 26;
-
-let prosjek = (num1 + num2 + num3) / 3;
-console.log(`Prosjek brojeva ${num1}, ${num2} i ${num3} je ${prosjek.toFixed(2)}.`);
